aeef64d61be7dab1f75b28ee04407f13dfc88435
Biplob kafle <biplobkafle21@gmail.com>
2024-08-05 21:43:13 +0545

    setting states in signup

--- src/app/_components/signup.tsx
+++ src/app/_components/signup.tsx
@@ -1,26 +1,63 @@
-import React from 'react'
+
+import React, { useState } from "react";
 
 const Signup = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+  const [city, setCity] = useState("");
+
+  function handleOnClick(){
+    
+  }
+
   return (
-    <div className='login-container'>
-      <div className='login-form'>
+    <div className="login-container">
+      <div className="login-form">
+        <label htmlFor="">Enter emai addresss</label>
+        <input
+          type="text"
+          placeholder="Enter your  email.."
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+        />
+        <label htmlFor="">Enter password</label>
+        <input
+          type="password"
+          name=""
+          id=""
+          placeholder="enter password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        />
+        <label htmlFor="confirm-password" className="signup-label">
+          Confirm password
+        </label>
+        <input
+          type="password"
+          id="confirm-password"
+          className="signup-input"
+          placeholder="Confirm your password"
+          value={newPassword}
+          onChange={(event) => setNewPassword(event.target.value)}
+        />
 
-      <label htmlFor="">Enter emai addresss</label>
-      <input type="text" placeholder='Enter your  email..' />
-      <label htmlFor="">Enter password</label>
-      <input type="password" name="" id="" placeholder='enter password' />
-      <label htmlFor="confirm-password" className="signup-label">Confirm password</label>
-        <input type="password" id="confirm-password" className="signup-input" placeholder="Confirm your password" />
+        <label htmlFor="city" className="signup-label">
+          Enter city name
+        </label>
+        <input
+          type="text"
+          id="city"
+          className="signup-input"
+          placeholder="Enter your city..."
+          value={city}
+          onChange={(event) => setCity(event.target.value)}
+        />
 
-
-      <label htmlFor="city" className="signup-label">Enter city name</label>
-        <input type="text" id="city" className="signup-input" placeholder="Enter your city..." />
-
-      <button >SignUp</button>
-      
+        <button>SignUp</button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Signup
+export default Signup;
