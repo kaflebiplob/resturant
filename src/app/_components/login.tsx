c6777e73710ff6399edca609cea525429b7f6a42
Biplob kafle <biplobkafle21@gmail.com>
2024-08-07 08:23:55 +0545

    state management in login page

--- src/app/_components/login.tsx
+++ src/app/_components/login.tsx
@@ -1,20 +1,44 @@
-import React from 'react'
+import { tree } from "next/dist/build/templates/app-page";
+import React, { useState } from "react";
 
 const Login = () => {
-  return (
-    <div className='login-container'>
-      <div className='login-form'>
-     
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(false);
 
-      <label htmlFor="">Enter emai addresss</label>
-      <input type="text" placeholder='Enter your  email..' />
-      <label htmlFor="">Enter password</label>
-      <input type="password" name="" id="" placeholder='enter password' />
-      <button > Login</button>
-      
+  const handleError = () => {
+    if (!email || !password) {
+      setError(true);
+    } else {
+      setError(false);
+     
+    }
+  };
+  return (
+    <div className="login-container">
+      <div className="login-form">
+        <label htmlFor="">Enter emai addresss</label>
+        <input
+          type="text"
+          placeholder="Enter your  email.."
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+        />
+        {error && !email &&<span className="input-error">Please enter email.</span>}
+        <label htmlFor="">Enter password</label>
+        <input
+          type="password"
+          name=""
+          id=""
+          placeholder="enter password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        />
+        {error && !password && <span className="input-error">Please enter password.</span>}
+        <button onClick={handleError}> Login</button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
